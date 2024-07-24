e503e7429aee747e3d6e1dfb95ed28cc5bb25d6b
Andrey Bakin <andybulkin@yandex.ru>
2024-07-24 15:12:10 +0300

    роуты

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const userRouter = require('./routes/users');
+const bookRouter = require('./routes/books');
 
 dotenv.config();
 
 const app = express();
 
-const {
-    PORT = 3000,
-    API_URL = "http://127.0.0.1" } = process.env;
+const { PORT, API_URL } = process.env;
 
+app.use(userRouter);
+app.use(bookRouter);
 
 app.listen(PORT, () =>{
     console.log(`Сервер зпущен по адресу ${API_URL}:${PORT}`);
