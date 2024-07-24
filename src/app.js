8de9c05a53546a8277b1a22e573d0ed52b77b1e5
Andrey Bakin <andybulkin@yandex.ru>
2024-07-24 16:27:44 +0300

    middlewares

--- src/app.js
+++ src/app.js
@@ -2,15 +2,19 @@ const express = require('express');
 const dotenv = require('dotenv');
 const userRouter = require('./routes/users');
 const bookRouter = require('./routes/books');
-
+const notFound = require('./middlewares/notFound');
+const originalUrl = require('./middlewares/originslUrl');
+const app = express();
 dotenv.config();
 
-const app = express();
+
 
 const { PORT, API_URL } = process.env;
 
 app.use(userRouter);
 app.use(bookRouter);
+app.use(notFound);
+app.use(originalUrl);
 
 app.listen(PORT, () =>{
     console.log(`Сервер зпущен по адресу ${API_URL}:${PORT}`);
